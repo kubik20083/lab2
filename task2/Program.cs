16f40c762073ece5d95780eface5500776aee19b
kubik20083 <kubik20083@mail.ru>
2022-12-25 21:26:02 +0300

    Решение второй задачи, в первой удалил левые числа

---
+++ task2/Program.cs
@@ -0,0 +1,12 @@
+﻿System.Console.WriteLine("Программа выводит третью цифру заданного числа или сообщает, что третьей цифры нет.");  
+
+Console.Write("Введите число: ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+string numberStr = Convert.ToString(number);
+int numberDigit = numberStr.Length;
+
+if (numberDigit > 2) 
+Console.WriteLine($"Третья цифра введённого числа: {numberStr[2]}");
+
+else Console.WriteLine("Третьей цифры нет");
