443cad663b6f0ce8f417e85e1275478606af5520
kubik20083 <kubik20083@mail.ru>
2022-12-25 21:52:41 +0300

    Решение третьей задачи

---
+++ task3/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число для недели от 1 до 7: ");
+int x = Convert.ToInt32(Console.ReadLine());
+if (x < 1 || x > 7)
+System.Console.WriteLine("Дней недели всего 7, введите число от 1 до 7");
+else if (x > 5)
+System.Console.WriteLine("Выходной день");
+
+else
+System.Console.WriteLine("Рабочий день");
+
